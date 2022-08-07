7492a0517fc147310d818bb5feb2ec9419aead0b
Vitalij Ryndin <crashmax.dev@gmail.com>
2022-08-08 06:40:58 +0800

    feat: update styles, added download pic button

--- src/main.ts
+++ src/main.ts
@@ -1,23 +1,66 @@
-import './style.css'
 import { canvasOutliner } from './canvasOutliner';
+import './style.css';
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <canvas>
-  </canvas>
-  <input type="file" name="upload" accept=".png">
-`
+const overlayOpenFile = document.querySelector('#overlay-open-image')!;
+const overlayCanvas = document.querySelector('#overlay-canvas')!;
 
 const canvasNode = document.querySelector('canvas')!;
+const imageUpload = document.querySelector('#image-upload')!;
+const defaultImage = document.querySelector('#open-default-image')!;
+const saveImage = document.querySelector('#save-image')!;
+const backToHome = document.querySelector('#back-to-home')!;
 
-document.querySelector('input')!.addEventListener('change', (e: Event) => {
-  const target = e.target as HTMLInputElement;
-  if (!target?.files || target.files?.length === 0) return;
-  
-  const reader = new FileReader();
-  reader.readAsDataURL(target.files[0]);
-  reader.onload = (e: Event) => {
-    //@ts-ignore
-    canvasOutliner(canvasNode, e.target?.result);
-  };
+function toggleOverlay() {
+  overlayOpenFile.classList.toggle('hidden');
+  overlayCanvas.classList.toggle('hidden');
+}
+
+class ImageReader {
+  private reader: FileReader;
+  public fileName: string | undefined;
+
+  constructor() {
+    this.reader = new FileReader();
+  }
+
+  open(files: FileList | null): void {
+    if (!files || files.length === 0) return;
+
+    this.reader.readAsDataURL(files[0]);
+    this.reader.onload = (event) => {
+      this.fileName = files[0].name;
+      const file = event.target?.result as string;
+      canvasOutliner(canvasNode, file);
+      toggleOverlay();
+    }
+  }
+
+  save(): void {
+    const link = document.createElement('a');
+    link.download = this.fileName ?
+      `${this.fileName}-outline` :
+      'canvas-outline.png';
+    link.href = canvasNode.toDataURL();
+    link.click();
+    link.remove();
+  }
+}
+
+const imageReader = new ImageReader();
+
+imageUpload.addEventListener('change', (event) => {
+  imageReader.open((event.target as HTMLInputElement).files);
 })
 
+defaultImage.addEventListener('click', () => {
+  canvasOutliner(canvasNode, './pikachu.png');
+  toggleOverlay();
+})
+
+saveImage.addEventListener('click', () => {
+  imageReader.save();
+})
+
+backToHome.addEventListener('click', () => {
+  toggleOverlay();
+})
