99f727958e2c292372895a902ad41f59497a8c3d
Poletansky Viktor <poletansky@gmail.com>
2022-09-07 23:52:00 +0300

    rewrite fill loop with iters

--- packages/my-crate/src/lib.rs
+++ packages/my-crate/src/lib.rs
@@ -20,14 +20,6 @@ impl PrefixSum2D {
         return PrefixSum2D {data, width, height}
     }
 
-    // add val to [0..y, 0..x] subarray
-    fn add(&mut self, i: usize, j: usize, val: i32) {
-        if i >= self.height || j >= self.width {
-            return
-        }
-        self.add_unsafe(i.min(self.height-1), j.min(self.width-1), val);
-    }
-
     fn add_unsafe(&mut self, i: usize, j: usize, val: i32) {
         self.data[i * self.width + j] += val;
     }
@@ -63,30 +55,39 @@ pub fn outline(data: Uint8ClampedArray, width: usize, height: usize, stroke_widt
             let k = ((i * width + j) * 4) as u32;
             return data.get_index(k + 3) == 255
         })
-        .map(|(i, j)| {
+        .flat_map(|(i, j)| {
             let top = if i>stroke_width {i-stroke_width} else {0};
             let left = if j>stroke_width {j-stroke_width} else {0};
             let bottom = i+stroke_width+1;
             let right = j+stroke_width+1;
-            (top, left, bottom, right)
+            vec![
+                (top, left, 1),
+                (top, right, -1),
+                (bottom, left, -1),
+                (bottom, right, 1),
+            ]
         })
-        .for_each(|(top, left, bottom, right)| {
-            pref.add_unsafe(top, left,  1);
-            pref.add(top, right, -1);
-            pref.add(bottom, left, -1);
-            pref.add(bottom, right, 1);
+        .filter(|(i, j, _)| *i < height && *j < width)
+        .for_each(|(i, j, val)| {
+            pref.add_unsafe(i, j, val);
         });
     pref.fill_prefix_sums();
-    for i in 0..height {
-        for j in 0..width {
+    (0..height)
+        .into_iter()
+        .flat_map(|i| (0..width)
+            .into_iter()
+            .map(move |j| (i, j)))
+        .filter(|(i, j)| {
             let k = ((i * width + j) * 4) as u32;
-            if data.get_index(k + 3) == 255 || pref.get(i, j) == 0 {
-                continue;
-            }
+            data.get_index(k + 3) != 255 && pref.get(*i, *j) != 0
+        })
+        .map(|(i, j)| {
+            ((i * width + j) * 4) as u32
+        })
+        .for_each(|k| {
             data.set_index(k, r);
             data.set_index(k+1, g);
             data.set_index(k+2, b);
             data.set_index(k+3, 255);
-        }
-    }
+        })
 }
