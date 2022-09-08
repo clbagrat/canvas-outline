aca40dbdc18714fa63d74e3e890e94c9d9ebdd10
Poletansky Viktor <poletansky@gmail.com>
2022-09-08 23:02:29 +0300

    parametrize shader STUPIDLY DONT DO THIS AT HOME PLZ

--- packages/canvas-outline/src/index.ts
+++ packages/canvas-outline/src/index.ts
@@ -1,4 +1,4 @@
-function canvasOutliner(canvas: HTMLCanvasElement, targetSrc: string, _strokeWidth: number, _strokeColor: {r: number, g: number, b: number}) {
+function canvasOutliner(canvas: HTMLCanvasElement, targetSrc: string, strokeWidth: number, strokeColor: {r: number, g: number, b: number}) {
   const gl = canvas.getContext("webgl");
   const img = document.createElement('img');
   img.src = targetSrc;
@@ -8,88 +8,95 @@ function canvasOutliner(canvas: HTMLCanvasElement, targetSrc: string, _strokeWid
     return;
   }
 
-  const vertexShaderSource = `
-    attribute vec4 aVertexPosition;
-    attribute vec2 aTextureCoord;
-    varying highp vec2 vTextureCoord;
-    void main(void) {
-      gl_Position = aVertexPosition;
-      vTextureCoord = aTextureCoord;
-    }`;
-  const fragmentShaderSource = `
-    varying highp vec2 vTextureCoord;
-    uniform sampler2D uSampler;
-    const int width = 4;
-    void paint(void) {
-      for (int i = -width; i <= width; i++) {
-        for (int j = -width; j <= width; j++) {
-          if (texture2D(uSampler, vTextureCoord + vec2(float(i), float(j))/vec2(846.0,832.0)).w > 0.5) {
-            gl_FragColor = vec4(1.0, 0.0, 0.0, 0.5);
-            return;
-          }
-        }
-      }
-    }
-    void main(void) {
-      highp vec4 tex = texture2D(uSampler, vTextureCoord);
-      if (tex.w > 0.5) {
-        gl_FragColor = tex;
-        return;
-      }
-      gl_FragColor = vec4(vec3(1,1,1) - tex.xyz, 1.0);
-      paint();
-    }`;
-
-  const vertexShader = loadShader(gl, gl.VERTEX_SHADER, vertexShaderSource)!;
-  const fragmentShader = loadShader(gl, gl.FRAGMENT_SHADER, fragmentShaderSource)!;
-  const shaderProgram = gl.createProgram()!;
-  gl.attachShader(shaderProgram, vertexShader);
-  gl.attachShader(shaderProgram, fragmentShader);
-  gl.linkProgram(shaderProgram);
-
-  if (!gl.getProgramParameter(shaderProgram, gl.LINK_STATUS)) {
-    alert("Unable to initialize the shader program: " + gl.getProgramInfoLog(shaderProgram));
-    return;
-  }
-
-  // Collect all the info needed to use the shader program.
-  // Look up which attributes our shader program is using
-  // for aVertexPosition, aTextureCoord and also
-  // look up uniform locations.
-  const vertexPosition = gl.getAttribLocation(shaderProgram, "aVertexPosition");
-  const textureCoord = gl.getAttribLocation(shaderProgram, "aTextureCoord");
-  const programInfo = {
-    program: shaderProgram,
-    attribLocations: {},
-    uniformLocations: {
-      uSampler: gl.getUniformLocation(shaderProgram, "uSampler")!,
-    },
-  };
-
-  const positionBuffer = gl.createBuffer()!;
-  gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
-  gl.bufferData(gl.ARRAY_BUFFER, new Float32Array([
-    -1.0, -1.0,
-     1.0, -1.0,
-     1.0,  1.0,
-    -1.0,  1.0,
-  ]), gl.STATIC_DRAW);
-
-  const textureCoordBuffer = gl.createBuffer()!;
-  gl.bindBuffer(gl.ARRAY_BUFFER, textureCoordBuffer);
-  gl.bufferData(gl.ARRAY_BUFFER, new Float32Array([0.0, 0.0, 1.0, 0.0, 1.0, 1.0, 0.0, 1.0]), gl.STATIC_DRAW);
-
-  const indexBuffer = gl.createBuffer()!;
-  gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, indexBuffer);
-  gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, new Uint16Array([0, 1, 2, 0, 2, 3]), gl.STATIC_DRAW);
-
-  const texture = gl.createTexture()!;
-  gl.bindTexture(gl.TEXTURE_2D, texture);
-
   const image = new Image();
   image.onload = function() {
     canvas.width = image.width;
     canvas.height = image.height;
+
+    let start = performance.now();
+    const vertexShaderSource = `
+      attribute vec4 aVertexPosition;
+      attribute vec2 aTextureCoord;
+      varying highp vec2 vTextureCoord;
+      void main(void) {
+        gl_Position = aVertexPosition;
+        vTextureCoord = aTextureCoord;
+      }`;
+
+    const [r, g, b] = [strokeColor.r / 255, strokeColor.g / 255, strokeColor.b / 255];
+    const fragmentShaderSource = `
+      varying highp vec2 vTextureCoord;
+      uniform sampler2D uSampler;
+      const int width = ${strokeWidth};
+      void paint(void) {
+        for (int i = -width; i <= width; i++) {
+          for (int j = -width; j <= width; j++) {
+            if (distance(vec2(float(i), float(j)), vec2(0, 0)) > float(width)) {
+              continue;
+            }
+            if (texture2D(uSampler, vTextureCoord + vec2(float(i), float(j))/vec2(${image.width}, ${image.height})).w > 0.5) {
+              gl_FragColor = vec4(${r}, ${g}, ${b}, 1.0);
+              return;
+            }
+          }
+        }
+      }
+      void main(void) {
+        highp vec4 tex = texture2D(uSampler, vTextureCoord);
+        if (tex.w > 0.5) {
+          gl_FragColor = tex;
+          return;
+        }
+        gl_FragColor = vec4(vec3(1,1,1) - tex.xyz, 1.0);
+        paint();
+      }`;
+
+    const vertexShader = loadShader(gl, gl.VERTEX_SHADER, vertexShaderSource)!;
+    const fragmentShader = loadShader(gl, gl.FRAGMENT_SHADER, fragmentShaderSource)!;
+    const shaderProgram = gl.createProgram()!;
+    gl.attachShader(shaderProgram, vertexShader);
+    gl.attachShader(shaderProgram, fragmentShader);
+    gl.linkProgram(shaderProgram);
+
+    if (!gl.getProgramParameter(shaderProgram, gl.LINK_STATUS)) {
+      alert("Unable to initialize the shader program: " + gl.getProgramInfoLog(shaderProgram));
+      return;
+    }
+
+    // Collect all the info needed to use the shader program.
+    // Look up which attributes our shader program is using
+    // for aVertexPosition, aTextureCoord and also
+    // look up uniform locations.
+    const vertexPosition = gl.getAttribLocation(shaderProgram, "aVertexPosition");
+    const textureCoord = gl.getAttribLocation(shaderProgram, "aTextureCoord");
+    const programInfo = {
+      program: shaderProgram,
+      attribLocations: {},
+      uniformLocations: {
+        uSampler: gl.getUniformLocation(shaderProgram, "uSampler")!,
+      },
+    };
+
+    const positionBuffer = gl.createBuffer()!;
+    gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
+    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array([
+      -1.0, -1.0,
+      1.0, -1.0,
+      1.0,  1.0,
+      -1.0,  1.0,
+    ]), gl.STATIC_DRAW);
+
+    const textureCoordBuffer = gl.createBuffer()!;
+    gl.bindBuffer(gl.ARRAY_BUFFER, textureCoordBuffer);
+    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array([0.0, 0.0, 1.0, 0.0, 1.0, 1.0, 0.0, 1.0]), gl.STATIC_DRAW);
+
+    const indexBuffer = gl.createBuffer()!;
+    gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, indexBuffer);
+    gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, new Uint16Array([0, 1, 2, 0, 2, 3]), gl.STATIC_DRAW);
+
+    const texture = gl.createTexture()!;
+    gl.bindTexture(gl.TEXTURE_2D, texture);
+
     gl.viewport(0, 0, image.width, image.height);
     gl.bindTexture(gl.TEXTURE_2D, texture);
     gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, image);
@@ -114,28 +121,12 @@ function canvasOutliner(canvas: HTMLCanvasElement, targetSrc: string, _strokeWid
     gl.uniform1i(programInfo.uniformLocations.uSampler, 0);
 
     gl.drawElements(gl.TRIANGLES, 6, gl.UNSIGNED_SHORT, 0);
+
+    console.log(performance.now() - start);
   };
   image.src = targetSrc;
 
   gl.pixelStorei(gl.UNPACK_FLIP_Y_WEBGL, true);
-
-  // img.onload = () => {
-  //   const { width, height } = img;
-  //   canvas.width = width;
-  //   canvas.height = height;
-  //   ctx.drawImage(img, 0, 0, width, height);
-  //   const {data: data} = ctx.getImageData(0, 0, width, height);
-  //   let start = performance.now();
-  //   outline(
-  //     data,
-  //     width, height,
-  //     strokeWidth,
-  //     strokeColor.r, strokeColor.g, strokeColor.b,
-  //   );
-  //   let imageData = new ImageData(data, width);
-  //   ctx.putImageData(imageData, 0, 0);
-  //   console.log(performance.now() - start);
-  // };
 }
 function loadShader(gl: WebGLRenderingContext, type: number, source: string) {
   const shader = gl.createShader(type)!;
