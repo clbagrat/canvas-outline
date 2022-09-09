784b13d3c820c924def806e6a2686961095d4065
Poletansky Viktor <poletansky@gmail.com>
2022-09-09 21:39:53 +0300

    style

--- packages/canvas-outline/src/index.ts
+++ packages/canvas-outline/src/index.ts
@@ -115,9 +115,10 @@ function initOutliner(canvas: HTMLCanvasElement): (targetSrc: string, strokeWidt
 
       let start = performance.now();
 
+      gl.viewport(0, 0, image.width, image.height);
+
       const texture = gl.createTexture()!;
       gl.bindTexture(gl.TEXTURE_2D, texture);
-      gl.viewport(0, 0, image.width, image.height);
       gl.bindTexture(gl.TEXTURE_2D, texture);
       gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, image);
       gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR);
